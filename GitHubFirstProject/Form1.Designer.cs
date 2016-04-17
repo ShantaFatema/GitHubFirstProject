4c90e042c1afde3eff64a0c7577443d904fec3f6
ShantaFatema <shantafatema@outlook.com>
2016-04-16 21:37:10 -0700

    button replace

--- GitHubFirstProject/Form1.Designer.cs
+++ GitHubFirstProject/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 64);
+            this.button1.Location = new System.Drawing.Point(157, 133);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
